929952d4a1132726fb27c9b488458cdf8fed95af
Tranivic <victor.trani1@gmail.com>
2024-10-01 21:50:29 -0300

    modified orm files

--- prisma/migrations/20240920140306_/migration.sql
+++ prisma/migrations/20240920140306_/migration.sql
@@ -1,14 +1,12 @@
 -- CreateTable
-CREATE TABLE "task_queue" (
+CREATE TABLE "workers_queue" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "date" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "slug" TEXT NOT NULL,
+    "scrap_role" TEXT NOT NULL,
     "pick_date" DATETIME NOT NULL,
     "pick_location" TEXT NOT NULL,
     "pick_hour" TEXT NOT NULL,
-    "days_with_car" INTEGER NOT NULL,
+    "drop_date" DATETIME NOT NULL,
+    "drop_hour" TEXT NOT NULL,
     "car_group" TEXT
 );
-
--- CreateIndex
-CREATE UNIQUE INDEX "task_queue_slug_key" ON "task_queue"("slug");
