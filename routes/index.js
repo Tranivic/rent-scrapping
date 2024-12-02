f17e2aefd2ed00620b3a8e8c5d78352cf9df5f2d
Tranivic <victor.trani1@gmail.com>
2024-12-02 15:30:43 -0300

    changed name in scrap route

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const routes = require('express').Router();
-const scraper = require('@routes/scraper/index.js');
+const scrap = require('@routes/scrap/index.js');
 const workers = require('@routes/workers/index.js');
 
 routes.get('/', (req, res) => {
     res.status(200).json({ message: 'connected...' });
 });
 
-routes.use('/scraper', scraper);
+routes.use('/scrap', scrap);
 routes.use('/workers', workers);
 
 module.exports = routes;
--- routes/scrap/index.js
+++ routes/scrap/index.js
@@ -1,15 +1,7 @@
 const scraper = require('express').Router();
 const unidas = require('@controller/unidas');
 
-scraper.get('/', async (req, res) => {
-    try {
-        res.status(200).json({ status: 'sucess', msg: 'success' });
-    } catch (e) {
-        res.status(500).json({ status: 'error', msg: 'error' });
-    }
-});
-
-scraper.get('/scrap', async (req, res) => {
+scraper.get('/unidas', async (req, res) => {
     try {
         const response = await unidas.unidas_price();
         res.status(200).json(response);
